7e239148a2a0e4c98dec507b31acd1af8038c783
Karsten <karsten_bach@hotmail.com>
2020-03-03 14:37:52 +0000

    init after copying most stuff from tumorigenesis

---
+++ src/07_BatchCorrection.R
@@ -0,0 +1,48 @@
+# Load Data
+library(scran)
+library(Matrix)
+library(batchelor)
+library(BiocParallel)
+library(BiocSingular)
+
+# Read in Data
+sce <- readRDS("../data/Robjects/SCE_QC_norm.rds")
+		      
+# Remove Anything that failed second round of QC 
+qcMets <- read.csv("../data/Robjects/QC_Part2.csv",row.names=1,stringsAsFactors=FALSE)
+rmCells <- qcMets$barcode[qcMets$isDoubletFinal | qcMets$isRbc]
+
+sce <- sce[,!colnames(sce) %in% rmCells]
+# Split into batch1 and batch2 
+
+sce1 <- sce[,sce$Batch==1]
+sce2 <- sce[,sce$Batch==2]
+
+#  Compute HVGs
+# Compute highly variable genes per batch
+dec.var1 <- modelGeneVar(sce1)
+dec.var2 <- modelGeneVar(sce2)
+
+# Combine Var
+combVar <- combineVar(dec.var1,dec.var2)
+stopifnot(identical(rownames(combVar),rownames(sce)))
+combVar <- combVar[rowData(sce)$KeepForHvg,]
+hvgs <- getTopHVGs(combVar)
+
+# mnnCorrect
+param <- MulticoreParam(workers=4)
+
+set.seed(300)
+mnncor <- batchelor::fastMNN(sce2,sce1,
+		     BPPARAM=param,
+		     k=20,
+		     d=50,
+		     BSPARAM=IrlbaParam(deferred=TRUE),
+		     #                      BNPARAM=AnnoyParam(),
+		     cos.norm=FALSE, # because I did multiBatchNorm shouldn't be necessary
+		     subset.row=hvgs)
+
+# After correction
+out.final <- reducedDim(mnncor)
+saveRDS(out.final,"../data/Robjects/CorrectedPCA.rds")
+saveRDS(mnncor,"../data/Robjects/FullMNN.rds")
---
+++ src/10_Clustering.R
@@ -0,0 +1,33 @@
+# Clustering
+library(igraph)
+library(scran)
+library(Matrix)
+source("../../Tumorigenesis2018/combined_src/functions.R")
+
+# Load Data
+sce <- readRDS("../data/Robjects/SCE_QC_norm.rds")
+ump <- read.csv("../data/Robjects/UMAP_corrected.csv",stringsAsFactors=FALSE)[,-1]
+sce <- sce[,ump$barcode] # this ensure the right order and cells for the umap graph below
+
+# Graph
+umapgraph <- readRDS("../data/Robjects/UMAP_graphs.rds")
+igr.cor <- umapgraph[["Corrected"]]
+
+# ---- Based on UMAP Graph ----
+# Clustering with walktrap on the UMAP graph
+set.seed(42)
+ump.wktrp <- cluster_walktrap(igr.cor, steps=5)
+cl.ump.wktrp <- paste0("C",ump.wktrp$membership)
+
+
+# ---- Merge Clusters ----
+rmgenes <- rownames(sce)[!rowData(sce)$KeepForHvg]
+sce$Cluster <- cl.ump.wktrp
+m <- logcounts(sce)
+merged <- mergeCluster(m, factor(sce$Cluster), removeGenes=rmgenes, block=sce$Batch)
+sce$Cluster <- merged$NewCluster
+
+# ---- Save ----
+out <- colData(sce)
+out$Cluster <- paste0("C",as.numeric(out$Cluster))
+write.csv(out[,c("barcode","Cluster")],"../data/Robjects/Clusters.csv")
