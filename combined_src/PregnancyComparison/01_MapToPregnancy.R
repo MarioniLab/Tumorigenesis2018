c8e101a61ae9b638d03046e82c08d369301312bb
Karsten <karsten_bach@hotmail.com>
2020-04-01 15:36:32 +0100

    merged umap into mapping and set order to TP

--- combined_src/PregnancyComparison/01_MapToPregnancy.R
+++ combined_src/PregnancyComparison/01_MapToPregnancy.R
@@ -4,6 +4,10 @@ library(Matrix)
 library(batchelor)
 library(BiocParallel)
 library(BiocSingular)
+library(umap)
+library(igraph)
+source("../functions.R")
+
 
 # Read in Tumorigenesis Data
 sce <- readRDS("../../data/combined_Robjects/SCE_final.rds")
@@ -12,11 +16,23 @@ colnames(sce) <- sce$barcode # not sure why it wasn't like this in the first pla
 # Read in Pregnancy Data
 sce.g <- readRDS("../../../PregnancyTimecourse2019/data/Robjects/SCE_final.rds")
 
+# Subset to same genes
 genes <- intersect(rownames(sce),rownames(sce.g))
 sce <- sce[genes,]
 sce.g <- sce.g[genes,]
 
+# Sub Sample for testing
+sce <- sce[,sample(ncol(sce),5000)]
+sce.g <- sce.g[,sample(ncol(sce.g),5000)]
 
+# Scale normalization factors
+mBatch <- batchelor::multiBatchNorm(sce,sce.g)
+sce <- mBatch[[1]]
+sce.g <- mBatch[[2]]
+rm(mBatch)
+
+
+# Extract ColData and combine
 pD1 <- data.frame(colData(sce))
 pD2 <- data.frame(colData(sce.g))
 pD2$CellTypes <- paste0("G_",pD2$CellTypes)
@@ -25,10 +41,10 @@ pD2$Batch <- as.numeric(as.character(pD2$Batch)) + 3
 pD1$Batch <- as.numeric(as.character(pD1$Batch))
 
 cols <- intersect(colnames(pD1),colnames(pD2))
-cols <- cols[!grepl("UMAP",cols)]
+cols <- cols[!grepl("UMAP",cols)] # One of them had the UMAP in there
 pDout <- rbind(pD1[,cols],pD2[,cols])
 
-# Split into batch1 and batch2 
+# Split into the separate batches
 sce1 <- sce[,sce$Batch==1]
 sce2 <- sce[,sce$Batch==2]
 sce3 <- sce[,sce$Batch==3]
@@ -54,26 +70,46 @@ hvgs <- getTopHVGs(combVar)
 # mnnCorrect
 param <- MulticoreParam(workers=4)
 
+# Define merge order so that the pregnancy is mapped onto tumorigenesis
+# The reasoning is that the tumorigenesis is the most heterogeneous, e.g. tumor cells
+# If this is not the first one, these might (and will) be wrongly associated to what is the closest 
+# In the other dataset
+ord <- list(list(1:3),list(4:5))
+
 set.seed(300)
-mnncor <- batchelor::fastMNN(sce4,sce5,sce1,sce2,sce3,
+mnncor <- batchelor::fastMNN(sce1,sce2,sce3,sce4,sce5,
 		     BPPARAM=param,
 		     k=20,
 		     d=50,
+		     merge.order=ord,
 		     BSPARAM=IrlbaParam(deferred=TRUE),
 		     #                      BNPARAM=AnnoyParam(),
 		     cos.norm=TRUE, # prob necesarry as I didn't use multibatchnorm
 		     subset.row=hvgs,
 		     correct.all=TRUE)
 
-m <- cbind(logcounts(sce4),logcounts(sce5),logcounts(sce1),
-	   logcounts(sce2),logcounts(sce3))
+m <- cbind(logcounts(sce1),logcounts(sce2),logcounts(sce3),
+	   logcounts(sce4),logcounts(sce5))
 rownames(pDout) <- pDout$barcode
 pDout <- DataFrame(pDout[colnames(mnncor),])
 colData(mnncor) <- pDout
 assays(mnncor) <- list("logcounts"=m,
 		       "reconstructed"=assays(mnncor)[["reconstructed"]])
 
-# After correction
-out.final <- reducedDim(mnncor)
-saveRDS(out.final,"../../data/combined_Robjects/Pregnancy_CorrectedPCA.rds")
+# PCA
+m.cor <- reducedDim(mnncor)
+
+# Compute UMAP for whole dataset
+ump.cor <- umap(m.cor, random_state=42)
+out <- data.frame("barcode"=rownames(m.cor))
+out$UMAP1 <- ump.cor$layout[,1]
+out$UMAP2 <- ump.cor$layout[,2]
+
+# igr.cor <- get_umap_graph(ump.cor)
+
+# Save correction
+saveRDS(m.cor,"../../data/combined_Robjects/Pregnancy_CorrectedPCA.rds")
 saveRDS(mnncor,"../../data/combined_Robjects/Pregnancy_FullMNN.rds")
+# Save UMAP
+saveRDS(igr.cor,"../../data/combined_Robjects/PregnancyUMAP_graph.rds")
+# write.csv(out,file="../../data/combined_Robjects/PregnancyUMAP_corrected.csv")
