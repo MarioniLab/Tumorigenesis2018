023c560fab610631c97b5f0e56fd9a8f034e15dd
Karsten <karsten_bach@hotmail.com>
2020-05-19 15:57:45 +0100

    trying WOT

---
+++ src/DownstreamAnalysis/prepData_for_WOT.R
@@ -0,0 +1,31 @@
+library(scran)
+sce <- readRDS("../../data/Integrated/Robjects/SCE_combined_final.rds")
+pca.tum <- readRDS("../../data/Tumorigenesis/Robjects/CorrectedPCA.rds")
+ttime <- read.csv("../../data/Integrated/Robjects/TumorTime.csv",)[,-1]
+ttime$ptimeBin[grepl("TM",ttime$Condition)] <- 5
+
+library(dplyr)
+smry <- group_by(ttime,ptimeBin) %>%
+    summarize(time=round(mean(ptime),0))
+
+ttime <- left_join(ttime,smry)
+
+pD.t <- data.frame(colData(sce[,sce$Experiment=="Tumorigenesis"]))
+pD.t <- dplyr::right_join(pD.t,ttime)
+colnames(pD.t)[colnames(pD.t)=="barcode"] <- "id" # for WOT
+pca.tum <- pca.tum[pD.t$id,]
+
+write.csv(pca.tum,"../../data/Integrated/Tumor_Pca_for_WOT.txt",quote=FALSE,row.names=TRUE)
+write.csv(pD.t,"../../data/Integrated/Tumor_pD_for_WOT.txt",quote=FALSE,row.names=FALSE)
+
+# Pregnancy
+pca.preg <- readRDS("../../data/Pregnancy/Robjects/CorrectedPCA.rds")
+pD <- data.frame(colData(sce[,sce$Experiment=="Pregnancy"]))
+colnames(pD)[colnames(pD)=="barcode"] <- "id" # for WOT
+pca.preg <- pca.preg[pD$id,]
+pD$time <- as.numeric(as.character(plyr::mapvalues(pD$Condition,
+			   c("CTRL","4.5dG","9.5dG","14.5dG"),
+			   c(0,4.5,9.5,14.5))))
+
+write.csv(pca.preg,"../../data/Integrated/Preg_Pca_for_WOT.txt",quote=FALSE,row.names=TRUE)
+write.csv(pD,"../../data/Integrated/Preg_pD_for_WOT.txt",quote=FALSE,row.names=FALSE)
---
+++ src/DownstreamAnalysis/runWOT.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+import wot
+
+# Read in Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--pca',  
+                    help='Path to the batch corrected PCA')
+parser.add_argument('--meta', 
+                    help='Path to meta data')
+parser.add_argument('--out', 
+                    help='Path to out folder')
+
+#Parser
+args = parser.parse_args()
+PCA_PATH = args.pca
+OBS_PATH = args.meta
+OUT_DIR = args.out
+
+#Load Data
+adata = wot.io.read_dataset(path= PCA_PATH, obs=OBS_PATH, var=None)
+adata.shape
+
+otmodel = wot.ot.OTModel(adata, epsilon = 0.05, lambda1 = 1, lambda2 = 50, day_field='time', growth_iters=3, local_pca=0)
+
+otmodel.compute_all_transport_maps(tmap_out=OUT_DIR)
