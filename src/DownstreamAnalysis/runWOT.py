5d6b7a3cad3e22621a18601ff6efd4ddcb7e9141
Karsten Bach <karsten_bach@hotmail.com>
2020-06-15 07:38:50 +0100

    minor changes

--- src/DownstreamAnalysis/runWOT.py
+++ src/DownstreamAnalysis/runWOT.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import argparse
 
 import wot
 
@@ -12,17 +13,20 @@ parser.add_argument('--meta',
                     help='Path to meta data')
 parser.add_argument('--out', 
                     help='Path to out folder')
+parser.add_argument('--time', 
+                    help='Path to out folder',default='time')
 
 #Parser
 args = parser.parse_args()
 PCA_PATH = args.pca
 OBS_PATH = args.meta
 OUT_DIR = args.out
+TIME=args.time
 
 #Load Data
 adata = wot.io.read_dataset(path= PCA_PATH, obs=OBS_PATH, var=None)
 adata.shape
 
-otmodel = wot.ot.OTModel(adata, epsilon = 0.05, lambda1 = 1, lambda2 = 50, day_field='time', growth_iters=3, local_pca=0)
+otmodel = wot.ot.OTModel(adata, epsilon = 0.05, lambda1 = 1, lambda2 = 50, day_field=TIME, growth_iters=3, local_pca=0)
 
 otmodel.compute_all_transport_maps(tmap_out=OUT_DIR)
